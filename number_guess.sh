96c9df07e2fa66061716a1682c341bb3bfedaa4b
NadirAli90 <nadiraliunar360@gmail.com>
2023-12-21 06:09:36 -0500

    feat: user prompt and checking already user

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read  NAME
-random_number=$((1 + RANDOM % 1000))
+USER_IS=$($PSQL "SELECT user_id FROM users WHERE name = '$NAME'")
 
-GUESS
+if [[ -z $USER_IS ]]; then
+    INSERT_USER=$($PSQL "INSERT INTO users(name) VALUES ('$NAME')")
+    echo "Welcome, $NAME! It looks like this is your first time here."
+else
+    echo "Welcome back, $NAME! You have played <games_played> games, and your best game took <best_game> guesses."
+fi
+
+random_number=$((1 + RANDOM % 1000))
